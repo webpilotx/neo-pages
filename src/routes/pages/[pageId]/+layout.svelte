323e29c7b2d34aa708803ba3459338a043bad5e1
chientrm <chientrm@gmail.com>
2025-04-20 14:11:07 +0700

    feat: add layout and navigation for page management with edit, deployments, and settings links

---
+++ src/routes/pages/[pageId]/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let data;
+</script>
+
+<nav class="mt-4">
+  <ul class="list-disc pl-5">
+    <li>
+      <a
+        href={`/pages/${data.pageId}/edit`}
+        class="text-blue-500 hover:underline"
+      >
+        Edit Page
+      </a>
+    </li>
+    <li>
+      <a
+        href={`/pages/${data.pageId}/deployments`}
+        class="text-blue-500 hover:underline"
+      >
+        Deployments
+      </a>
+    </li>
+    <li>
+      <a
+        href={`/pages/${data.pageId}/settings`}
+        class="text-blue-500 hover:underline"
+      >
+        Settings
+      </a>
+    </li>
+  </ul>
+</nav>
+
+<slot />
